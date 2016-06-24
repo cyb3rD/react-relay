f875a3475e896253f3c455d16fcc1bd3c9e92280
Nikolay Ignatyev <noilgt@gmail.com>
2016-06-24 10:57:47 +0300

    Added MongoDB endpoint

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 import express from 'express';
 
-let app =  express();
+import {MongoClient} from 'mongodb';
 
+let server = {
+  port: 7000,
+  mongodbConnect: 'mongodb://react-relay:react-admin@ds023064.mlab.com:23064/react-relay-course'
+}
+
+let app =  express();
 app.use(express.static('public'));
 
-app.listen(7000);
+let db;
+MongoClient.connect(server.mongodbConnect, (err, database) => {
+  if (err) throw err;
+
+  db = database;
+  app.listen(server.port, () => console.log('Listening on port: ', server.port));
+
+});
+
+app.get("/data/links", (req, res) => {
+
+  db.collection("links").find({}).toArray((err, links) => {
+    if (err) throw err;
+
+    res.json(links);
+  });
+
+});
